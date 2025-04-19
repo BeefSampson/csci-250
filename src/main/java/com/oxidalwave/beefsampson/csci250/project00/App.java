cac8d57140ce32ba27956ca22261a4494471db2f
OxidalWave <github@oxidalwave.com>
2025-04-19 18:49:41 -0500

    Init for Brenny

---
+++ src/main/java/com/oxidalwave/beefsampson/csci250/project00/App.java
@@ -0,0 +1,10 @@
+package com.oxidalwave.beefsampson.csci250.project00;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
