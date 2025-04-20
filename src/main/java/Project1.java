f06a0a1318e61bf936dd3e16f20c0f2f1861043a
OxidalWave <github@oxidalwave.com>
2025-04-19 19:22:02 -0500

    Simplify execution

--- src/main/java/Project1.java
+++ src/main/java/Project1.java
@@ -1,9 +1,4 @@
-package csci250.project00;
-
-/**
- * Hello world!
- */
-public class App {
+public class Project1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
